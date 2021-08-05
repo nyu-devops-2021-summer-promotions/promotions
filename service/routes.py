d3694c0f6ac7e1a1130828c453f948cf8c89cb39
prateek mahindra <prateekmahindra9@gmail.com>
2021-08-05 11:46:49 -0400

    Debugged the issues provided by Shiho

--- service/routes.py
+++ service/routes.py
@@ -49,7 +49,6 @@ api = Api(app,
           default='promotions',
           default_label='Promotion operations',
           doc='/apidocs', # default also could use doc='/apidocs/'
-          authorizations=authorizations,
           prefix='/api'
          )
 
