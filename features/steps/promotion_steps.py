7f1dd264168e257740d2e0806432c2c8bce992ae
Meng Ting Chien <mengtingchien@mengde-mbp.myfiosgateway.com>
2021-08-06 16:11:34 -0400

    add json header

--- features/steps/promotion_steps.py
+++ features/steps/promotion_steps.py
@@ -23,7 +23,7 @@ def step_impl(context):
     """ Delete all Promotions and load new ones """
     headers = {'Content-Type': 'application/json'}
     # list all of the promotions and delete them one by one
-    context.resp = requests.get(context.base_url + '/promotions')
+    context.resp = requests.get(context.base_url + '/promotions', headers=headers)
     expect(context.resp.status_code).to_equal(200)
     for promotion in context.resp.json():
         context.resp = requests.delete(context.base_url + '/promotions/' + str(promotion["id"]), headers=headers)
