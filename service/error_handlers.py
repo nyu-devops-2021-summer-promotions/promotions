80f3dc9be68aa4dffb8b05c6a128801604aa3933
Meng Ting Chien <mengtingchien@Mengde-MacBook-Pro.local>
2021-08-09 18:07:54 -0400

    add 500 back

--- service/error_handlers.py
+++ service/error_handlers.py
@@ -7,7 +7,24 @@ from service.models import DataValidationError
 from . import app, status
 
 
+@app.errorhandler(DataValidationError)
+def request_validation_error(error):
+    """ Handles Value Errors from bad data """
+    return bad_request(error)
 
 
 
 
+@app.errorhandler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    """ Handles unexpected server error with 500_SERVER_ERROR """
+    message = str(error)
+    app.logger.error(message)
+    return (
+        jsonify(
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error="Internal Server Error",
+            message=message,
+        ),
+        status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
